9a59fc6c9907b77bdc5d5e92f6e0f2d97df3d0eb
Krishna Shrestha <krishnashrestha49@gmail.com>
2021-08-25 23:02:45 +0545

    Added Create and Save Changes Method on Commander Repo

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -4,6 +4,11 @@ using Commander.Models;
 namespace Commander.Data {
     public class MockCommanderRepo : ICommanderRepo
     {
+        public void CreateCommand(Command command)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
             var commands = new List<Command> {
@@ -18,6 +23,11 @@ namespace Commander.Data {
         public Command GetCommandById(int id)
         {
             return new Command{ Id = 0, HowTo = "Boil an egg", Line="Boil Water", Platform="Kettle & Pan" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
